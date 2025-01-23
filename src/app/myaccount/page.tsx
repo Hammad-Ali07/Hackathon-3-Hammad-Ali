2a3a76ef58fe295b46af824d3548cdf6b7d280ff
Hammad-Ali07 <hammii272878@gmail.com>
2025-01-23 06:17:20 +0500

    add files

--- src/app/myaccount/page.tsx
+++ src/app/myaccount/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Header from '../components/header';
 import Field from '../components/Feild';
 
 function MyAccount() {
